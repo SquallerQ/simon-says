03b8581e90155cf0344acb195e3cdb840e5faa06
zSqualler <john199502031@gmail.com>
2025-01-08 21:00:54 +0100

    feat: add output and level-information menu

--- script.js
+++ script.js
@@ -1,81 +1,139 @@
-const fullCharactersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0,"q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+const easyLevelArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const mediumLevelArray = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+const hardLevelArray = [1,2,3,4,5,6,7,8,9,0,"q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
 
-const body = document.querySelector('body');
+const body = document.querySelector("body");
+
+const gameInformation = {
+  round: 1,
+  difficult: 'easy',
+
+}
 
 
 
 function renderPage() {
+  renderLevelInformation(gameInformation);
 
-  
 }
+renderPage()
 
-function createInputField () {
+function createOutputField() {
+  const divForOutput = document.createElement("div");
+  divForOutput.classList.add("output__block");
+  body.append(divForOutput);
+
+  const output = document.createElement("div");
+  output.classList.add("output");
+  divForOutput.append(output);
+}
+createOutputField();
+
+
+function createInputField() {
   const divForInput = document.createElement("div");
-  divForInput.classList.add('inputParent')
+  divForInput.classList.add("input__block");
   body.append(divForInput);
-  
-  
-  const input = document.createElement('div');
+
+  const input = document.createElement("div");
   input.classList.add("input");
   divForInput.append(input);
-
 }
-createInputField()
+createInputField();
 
 
 
-function createVirtualKeyboard () {
+
+function createVirtualKeyboard(characterArray) {
   const divForKeyboard = document.createElement("div");
   divForKeyboard.classList.add("divForKeyboard");
   body.append(divForKeyboard);
 
-  for (let i = 0; i < fullCharactersArray.length; i++) {
+  for (let i = 0; i < characterArray.length; i++) {
     const key = document.createElement("div");
-    key.classList.add('key');
-    key.innerText = fullCharactersArray[i];
+    key.classList.add("key");
+    key.innerText = characterArray[i];
     divForKeyboard.append(key);
   }
-
 }
 
-createVirtualKeyboard()
+createVirtualKeyboard(hardLevelArray);
 
+const keyBoard = document.querySelector(".divForKeyboard");
+// console.log(keyBoard);
 
-
-  const keyBoard = document.querySelector(".divForKeyboard");
-  // console.log(keyBoard);
-  
-  if (keyBoard) {
-    keyBoard.addEventListener("click", listenClick);
-  }
+if (keyBoard) {
+  keyBoard.addEventListener("click", listenClick);
+}
 
 function listenClick(event) {
   let clickedButton = event.target;
-  if (clickedButton.classList.contains('key')) {
-    changeInput(clickedButton);    
+  if (clickedButton.classList.contains("key")) {
+    changeInput(clickedButton);
   } else {
     return;
   }
-  clickedButton = '';
+  clickedButton = "";
 }
 
-function changeInput (key) {
-  const input = document.querySelector('.input')
+function changeInput(key) {
+  const input = document.querySelector(".input");
   let inputValue = input.innerText;
   let keyValue = key.innerText;
   console.log(inputValue);
   console.log(keyValue);
-  
+
   inputValue = inputValue + keyValue;
   if (input) {
     input.innerText = inputValue;
   }
-} 
-
-function createLevelInformation () {
-  const divForLevelInformation = document.createElement("div");
-  divForLevelInformation.classList.add("divForLevelInformation");
-  body.append(divForLevelInformation);
 }
 
-createLevelInformation()
+function renderLevelInformation(_gameInformation) {
+  const levelInformation = document.createElement("div");
+  levelInformation.classList.add("level-information");
+  body.append(levelInformation);
+
+  const levelDifficultBlock = document.createElement("div");
+  levelDifficultBlock.classList.add("level-information__difficult-items");
+  levelInformation.append(levelDifficultBlock);
+
+  
+  const levelDifficultEasy = document.createElement("div");
+  const levelDifficultMedium = document.createElement("div");
+  const levelDifficultHard = document.createElement("div");
+
+  levelDifficultEasy.innerText = "Easy";
+  levelDifficultMedium.innerText = "Medium";
+  levelDifficultHard.innerText = "Hard";
+
+  levelDifficultEasy.classList.add("level-information__difficult-item");
+  levelDifficultMedium.classList.add("level-information__difficult-item");
+  levelDifficultHard.classList.add("level-information__difficult-item");
+
+  levelDifficultBlock.append(levelDifficultEasy);
+  levelDifficultBlock.append(levelDifficultMedium);
+  levelDifficultBlock.append(levelDifficultHard);
+
+
+  levelDifficultEasy.classList.remove('level-information__difficult-item--active')
+  levelDifficultMedium.classList.remove('level-information__difficult-item--active')
+  levelDifficultHard.classList.remove('level-information__difficult-item--active')
+  
+  if (gameInformation.difficult === 'easy') {
+    levelDifficultEasy.classList.add("level-information__difficult-item--active");
+  } else if (gameInformation.difficult === 'medium') {
+    levelDifficultMedium.classList.add("level-information__difficult-item--active");
+  } else if (gameInformation.difficult === 'hard') {
+    levelDifficultHard.classList.add("level-information__difficult-item--active");
+  }
+
+
+  const currentRound = document.createElement("div");
+  currentRound.classList.add("level-information__round");
+  currentRound.innerText = `Current round: ${gameInformation.round}/5`;
+  levelInformation.append(currentRound);
+
+}
+
+
